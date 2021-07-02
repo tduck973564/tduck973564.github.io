3832ce9bd9474b6ca2caebf7f6b674d5575a7281
tduck973564 <tduck973564@gmail.com>
2021-07-03 00:39:50 +1000

    fixed navbar not showing on window resize

--- docs/toggleNavVisibility.js
+++ docs/toggleNavVisibility.js
@@ -1,6 +1,5 @@
 function toggleNavVisibility() {
     let nav = document.getElementById("nav");
-    console.log(nav);
     if (nav.style.display === "none") {
         nav.style.display = "block"
     }
@@ -8,3 +7,16 @@ function toggleNavVisibility() {
         nav.style.display = "none";
     }
 }
+
+function makeNavVisibleOnMediaQuery(x) {
+    let nav = document.getElementById("nav");
+    if (!x.matches) {
+        nav.style.display = "block";
+    } else {
+        nav.style.display = "none";
+    }
+}
+
+let screenSizeMediaQuery = window.matchMedia("(max-width: 1200px)");
+makeNavVisibleOnMediaQuery(screenSizeMediaQuery);
+screenSizeMediaQuery.addListener(makeNavVisibleOnMediaQuery);
